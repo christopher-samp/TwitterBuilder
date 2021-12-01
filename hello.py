8dd31054bd017ceb32f8a019e8591779b0a0d616
Christopher Robert Samp <sampcr@rose-hulman.edu>
2021-11-30 18:52:07 -0600

    added a get_tweets callout

--- hello.py
+++ hello.py
@@ -60,11 +60,15 @@ def recent_searches():
     #print(client.get_tweet(1462814411970723844, user_auth=True))
     #print(api.get_saved_searches())
     #print(len(api.get_saved_searches()))
-    theTweet = client.get_tweet(1462814411970723844, user_auth=True).data
+    theTweet = client.get_tweet(id=1462814411970723844, user_auth=True, expansions='author_id', media_fields='preview_image_url', tweet_fields="public_metrics")
+    UsersId = theTweet.includes['users'][0].id
     
+    theUser = api.get_user(id=UsersId, user_auth=True, user_fields=['profile_image_url'])._json
+    print(theTweet)
     tweet = {
-        "data": theTweet.text,
-        "id": theTweet.id
+        "data": theTweet.data.text,
+        "id": theTweet.data.id,
+        "profile_image_https": theUser['profile_image_url_https']
     }
     tweetlist = [tweet]
 
@@ -104,5 +108,40 @@ def get_user_by_id():
     userList = [user]
 
     return jsonify(userList)
+
+@app.route('/GetTweets')
+def get_tweets():
+    consumer_key = "niFrGMblGwSn7TzYPntdFqeEr"
+    consumer_secret = "T8d4UU9vahI4oUzVUX9Cxh2srs4axKXEZ6rbr0QvwPpOFfL52g"
+
+    # Your account's (the app owner's account's) access token and secret for your
+    # app can be found under the Authentication Tokens section of the
+    # Keys and Tokens tab of your app, under the
+    # Twitter Developer Portal Projects & Apps page at
+    # https://developer.twitter.com/en/portal/projects-and-apps
+    access_token = "369867339-8drZ7FPKl8BdZjgJdlnj07x82r5SWz4derBVfN8S"
+    access_token_secret = "bj3lqGkWCDABRMVA2LewMLvmd1K3YLPgcfNTUBMQ1FhMP"
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+    client = tweepy.Client(auth, consumer_key, consumer_secret, access_token, access_token_secret)
+
+    TweetList = []
+    for status in tweepy.Cursor(api.search_tweets, "Tweepy",
+                            count=100).items(1):
+        print(status._json['text'])
+        tweet = {
+            "data": status._json['text'],
+            "id": status._json['id'],
+            "retweets": status._json['retweet_count'],
+            "favorites": status._json['favorite_count'],
+            "date": status._json['created_at'],
+            "profile_image_url_https": status._json['user']['profile_image_url_https']
+        }
+        TweetList.append(tweet)
+        
+    return jsonify(TweetList)
     
     
